90aa1548267f7a4a5cfaf8252400118c6a01f50b
neoFelhz <neoFelhz@gmail.com>
2017-06-28 18:38:37 +0800

    feat(function): add copress html support

--- functions.php
+++ functions.php
@@ -117,4 +117,36 @@ add_action('save_post', 'clear_db_cache_archives_list'); // 新发表文章/修�
          wp_mail( $to, $subject, $message, $headers );
      }
  }
+
+//Compress the HTML code
+
+function wp_compress_html(){
+    function wp_compress_html_main ($buffer){
+        $initial=strlen($buffer);
+        $buffer=explode("<!--compressed-html-->", $buffer);
+        $count=count ($buffer);
+        for ($i = 0; $i <= $count; $i++){
+            if (stristr($buffer[$i], '<!--wp-compress-html no compression-->')) {
+                $buffer[$i]=(str_replace("<!--wp-compress-html no compression-->", " ", $buffer[$i]));
+            } else {
+                $buffer[$i]=(str_replace("\t", " ", $buffer[$i]));
+                $buffer[$i]=(str_replace("\n\n", "\n", $buffer[$i]));
+                $buffer[$i]=(str_replace("\n", "", $buffer[$i]));
+                $buffer[$i]=(str_replace("\r", "", $buffer[$i]));
+                while (stristr($buffer[$i], '  ')) {
+                    $buffer[$i]=(str_replace("  ", " ", $buffer[$i]));
+                }
+            }
+            $buffer_out.=$buffer[$i];
+        }
+        $final=strlen($buffer_out);   
+        $savings=($initial-$final)/$initial*100;   
+        $savings=round($savings, 2);   
+        $buffer_out.="\n<!--Before: $initial bytes; Now: $final bytes; Saving: $savings% -->";   
+    return $buffer_out;
+}
+ob_start("wp_compress_html_main");
+}
+add_action('get_header', 'wp_compress_html');
+
 ?>
